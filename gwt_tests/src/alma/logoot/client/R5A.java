bd7f7d85fd3522b17ad73edbe6b6b92e7225461a
driz764 <drizz764@gmail.com>
2012-01-14 00:54:54 +0100

    Debug. ctrl+c / v ok now.

--- gwt_tests/src/alma/logoot/client/R5A.java
+++ gwt_tests/src/alma/logoot/client/R5A.java
@@ -14,6 +14,8 @@ import alma.logoot.shared.diff_match_patch;
 import alma.logoot.shared.diff_match_patch.Diff;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.ui.FlexTable;
@@ -50,120 +52,48 @@ public class R5A implements EntryPoint {
 		LogootIdContainer id = new LogootIdContainer();
 		id.add(new LogootIdentifier(15, 1, 1));
 		id.add(new LogootIdentifier(5, 1, 2));
-		// textArea.addKeyPressHandler(new KeyPressHandler() {
-		// @Override
-		// public void onKeyPress(KeyPressEvent event) {
-		// System.out.println("onKeypress");
-		// int pos=textArea.getCursorPos();
-		// LogootIdContainer p=idTable.get(pos);
-		// LogootIdContainer q=idTable.get(pos+1);
-		// ArrayList<LogootIdContainer> idList=
-		// LogootEngine.generateLineIdentier(p, q, 1, ID);
-		// char content=(char) event.getUnicodeCharCode();
-		// Operation op=new OpInsert(idList.get(0), content);
-		// idTable.add(pos+1,idList.get(0));
-		// log(pos,content,idList.get(0),p,q);
-		// send(op);
-		// }
-		// });
 		textArea.addKeyUpHandler(new KeyUpHandler() {
 			@Override
 			public void onKeyUp(KeyUpEvent event) {
-				diff_match_patch diffEngine = new diff_match_patch();
-				LinkedList<Diff> diff = diffEngine.diff_main(old,
-						textArea.getText(), false);
-				System.out.println("=======Event : diff(\"" + old + "\",\""
-						+ textArea.getText() + "\")===========");
-				old = textArea.getText();
-				System.out.println("=>" + diff);
-				alma.logoot.shared.Patch patch = new alma.logoot.shared.Patch();
-				int index = 0;
-				for (Diff d : diff) {
-					System.out.println("\t" + d);
-					if (d.operation == alma.logoot.shared.diff_match_patch.Operation.EQUAL) {
-						index += d.text.length();
-					} else if (d.operation == alma.logoot.shared.diff_match_patch.Operation.INSERT) {
-						LogootIdContainer p = idTable.get(index);
-						LogootIdContainer q = idTable.get(index + 1);
-						ArrayList<LogootIdContainer> idList = LogootEngine
-								.generateLineIdentier(p, q, d.text.length(), ID);
-						// Mise a jour idTable
-						idTable.addAll(index + 1, idList);
-						// Creation opérations
-						int i = 0;
-						for (LogootIdContainer lic : idList) {
-							patch.add(new OpInsert(lic, d.text.charAt(i)));
-							log(index, d.text.charAt(i), lic, p, q);
-							i++;
-						}
-
-						index += d.text.length();
-					} else { // DELETE
-						// Recuperation des LogootIdContainer
-						LogootIdContainer position = idTable.get(index + 1);
-						// Mise a jour idTable
-						idTable.remove(position);
-						// Creation opération
-						alma.logoot.shared.Operation op = new OpDelete(position);
-						// Ajout dans le patch
-						patch.add(op);
-					}
-				}
-				send(patch);
-
-				// int pos=textArea.getCursorPos();
-				// LogootIdContainer p=idTable.get(pos);
-				// LogootIdContainer q=idTable.get(pos+1);
-				// ArrayList<LogootIdContainer> idList=
-				// LogootEngine.generateLineIdentier(p, q, 1, ID);
-				// char content=(char) event.getUnicodeCharCode();
-				// Operation op=new OpInsert(idList.get(0), content);
-				// idTable.add(pos+1,idList.get(0));
-				// log(pos,content,idList.get(0),p,q);
-				// send(op);
+				System.out.println("=======Event KeyUp=========");
+				onEvent();
+			}
+		});
+		textArea.addChangeHandler(new ChangeHandler() {
+			
+			@Override
+			public void onChange(ChangeEvent event) {
+				System.out.println("=======Event Change=========");
+				onEvent();
 			}
 		});
 
-		/*
-		 * textArea.addKeyUpHandler(new KeyUpHandler() {
-		 * 
-		 * @Override public void onKeyUp(KeyUpEvent event) { // Création
-		 * identifiantLogoot // Send int pos=textArea.getCursorPos();
-		 * LogootIdContainer p=idTable.get(pos-1); LogootIdContainer
-		 * q=idTable.get(pos); ArrayList<LogootIdContainer> idList=
-		 * LogootEngine.generateLineIdentier(p, q, 1, ID);
-		 * System.out.println(event.getNativeEvent().getString()); char
-		 * content=(char) event.getNativeKeyCode();
-		 * System.out.println("="+content+"="); Operation op=new
-		 * OpInsert(idList.get(0), content); send(op);
-		 * idTable.add(pos,idList.get(0)); log(pos,content,idList.get(0),p,q);
-		 * //
-		 * log.setText(pos+"-"+((char)event.getNativeKeyCode())+"-"+idList.get
-		 * (0)+"entre (" // +p+"-"+q+")\n"+log.getText()); } });
-		 */
 	}
 
 	protected void send(Patch patch) {
-		System.out.println("= send patch =");
+		System.out.println("= send patch =\n=>" + patch);
 		receive(patch);
 	}
 
 	private void receive(Patch patch) {
-		System.out.println("= receive patch =");
+		System.out.println("= receive patch with " + patch.size() + "=");
 		for (Operation o : patch) {
-			receive(o);
+			try {
+				receive(o);
+			} catch (Exception e) {
+				// FIXME Comprendre et traiter cette exception
+			}
 		}
 	}
 
-	
 	@SuppressWarnings("unused")
 	private void send(Operation op) {
-		System.out.println("= send("+op+") =");
+		System.out.println("= send(" + op + ") =");
 		receive(op);
 	}
 
 	private void receive(Operation op) {
-		System.out.println("= receive("+op+") =");
+		System.out.println("= receive(" + op + ") =");
 		if (op.isIns()) {
 			OpInsert o = (OpInsert) op;
 			int index = 0;
@@ -180,7 +110,7 @@ public class R5A implements EntryPoint {
 			int index = idTableRec.indexOf(op.getPosition());
 			idTableRec.remove(index);
 			StringBuffer sb = new StringBuffer(textReceiver.getText());
-			sb.deleteCharAt(index-1);
+			sb.deleteCharAt(index - 1);
 			textReceiver.setText(sb.toString());
 		}
 	}
@@ -217,6 +147,56 @@ public class R5A implements EntryPoint {
 		log.setText(logRow, 3, prec.toString());
 		log.setText(logRow, 4, suiv.toString());
 		++logRow;
+	}
+
+	/**
+	 * 
+	 */
+	private void onEvent() {
+		diff_match_patch diffEngine = new diff_match_patch();
+		LinkedList<Diff> diff = diffEngine.diff_main(old,
+				textArea.getText(), false);
+		System.out.println("Diff : " + diff);
+		old = textArea.getText();
+		
+		int index = 0;
+		for (Diff d : diff) {
+			if (d.operation == alma.logoot.shared.diff_match_patch.Operation.EQUAL) {
+				index += d.text.length();
+			} else {
+				alma.logoot.shared.Patch patch = new alma.logoot.shared.Patch();
+				if (d.operation == alma.logoot.shared.diff_match_patch.Operation.INSERT) {
+
+					LogootIdContainer p = idTable.get(index);
+					LogootIdContainer q = idTable.get(index + 1);
+					ArrayList<LogootIdContainer> idList = LogootEngine
+							.generateLineIdentier(p, q,
+									d.text.length(), ID);
+					// Mise a jour idTable
+					idTable.addAll(index + 1, idList);
+					// Creation opérations
+					int i = 0;
+					for (LogootIdContainer lic : idList) {
+						alma.logoot.shared.Operation op = new OpInsert(
+								lic, d.text.charAt(i));
+						patch.add(op);
+						log(index, d.text.charAt(i), lic, p, q);
+						i++;
+					}
+					index += d.text.length();
+				} else { // DELETE
+					for (int i = 0; i < d.text.length(); i++) {
+						LogootIdContainer position = idTable
+								.get(index + 1);
+						idTable.remove(position);
+						alma.logoot.shared.Operation op = new OpDelete(
+								position);
+						patch.add(op);
+					}
+				}
+				send(patch);
+			}
+		}
 	}
 
 }
