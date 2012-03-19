86d831c560a9506c8b0aca9aa2131444a77b409c
Adrien Bougouin <adrienbougouin@new-host.home>
2012-03-19 23:07:32 +0100

    supression faite mais il reste un bug a la suppression du premier caractere + remonte des chaines de caractere en variable globale + debut d'integration du logoot

---
+++ span_logoot_test/logoot.js
@@ -0,0 +1,146 @@
+//! Boundary for LineId Generator
+Logoot.BOUNDARY = undefined;
+
+/*!
+ * \class   Logoot
+ * \brief   Logoot algorithm implementation.
+ *
+ * Logoot algorithm implementation.
+ */
+function Logoot() {
+}
+
+/*!
+ * \brief   Generation of a position, logoot algorithm.
+ *
+ * \param   previousLineId  The previous LineId.
+ * \param   nextLineId      The next LineId.
+ * \param   N               Number of positions generated.
+ * \param   boundary        
+ * \param   rep             Unique user replica.
+ * \param   clock           Unique user clock at this time.
+ * \return  List of N LineId between previous and next LineId.
+ */
+Logoot.generateLineId = function(previousLineId, nextLineId, N,
+    boundary, replica, clock) {
+  var prefPrev = 0;
+  var prefNext = 0;
+  var index = 0;
+  var interval = 0;
+
+  // Compute index to ensure between p and q you could put N new LineId.
+  while (interval < N) {
+    index ++;
+
+    // Compute prefix
+    prefPrev = Logoot.prefix(previousLineId, index);
+    prefNext = Logoot.prefix(nextLineId, index);
+
+    // Compute interval
+    interval = prefNext - prefPrev - 1;
+  }
+
+  // Construct Indentifier.
+  var step = interval/N;
+  step = (boundary) ? Math.min(Math.round(step), boundary) : Math.round(step);
+  var r = prefPrev;
+  var list = [];
+
+  // -- DEBUG START
+  /*
+  console.log('prefixPreviousLineId:' + prefPrev);
+  console.log('prefixNextLineId:' + prefNext);
+  console.log('interval:' + interval);
+  console.log('step:' + step);
+  //*/
+  // -- DEBUG END
+
+  for (var j = 1; j <= N; j++) {
+    list.push(Logoot.constructLineId(r + rand(1, step), previousLineId,
+          nextLineId, replica, clock));
+
+    r += step;
+  }
+
+  return list;
+}
+
+/*!
+ * \brief   Compute the prefix from a LineId.
+ *
+ * Returns new number in same base as lineId position integer. It returns
+ * \c index first position.getInt from lineId. Each position.getInt is
+ * put in same base as \BASE.
+ *
+ * \param   lineId  The line id to compute prefix.
+ * \param   index   Index first position.getInt of lineId.
+ * \return  Prefix of lineId.
+ */
+Logoot.prefix = function(lineId, index) {
+  var result = '';
+  var min = Math.min(index, lineId.length());
+  
+  // Get each position.getInt and put it in right \c BASE.
+  for (var id = 0; id < min; ++ id) {
+    result += str_pad(lineId.getPosition(id).getInt().toString(), DIGIT, '0',
+        'STR_PAD_LEFT');
+  }
+
+  // If index is bigger than positions available, fill with 0.
+  while (min < index) {
+    result += str_pad('', DIGIT, '0');
+    ++ min;
+  }
+
+  return Number(result);
+}
+
+/*!
+ * \brief   Generate randomly a LineId.
+ *
+ * \param   r             value to generate LineId.
+ * \param   startLineId   LineId from generate r.
+ * \param   endLineId     LineId to generate r.
+ * \param   replica       User unqiue replica.
+ * \param   clock         User clock (at this time).
+ */
+Logoot.constructLineId = function(r, startLineId, endLineId, replica, clock) {
+  var strR = r.toString();
+
+  // Cut strR on (DIGIT) to get each chunk. if strR isn't cutable on DIGIT,
+  // add needed 0 at left.
+  var lastChunkSize = strR.length % DIGIT;
+  if (lastChunkSize != 0) {
+    var zeroToAdd = str_pad('', DIGIT - lastChunkSize, '0');
+    strR = zeroToAdd + strR;
+  }
+
+  var chunksR = str_split(strR, DIGIT);
+  var lineId = new LineId();
+
+  // Generate position of lineId.
+  for (var i in chunksR) {
+    var position;
+    var d = Number(chunksR[i]);
+
+    if (i < startLineId.length()
+        && d == startLineId.getPosition(i).getInt()) {
+      position = new Position(d,
+          startLineId.getPosition(i).getReplica(),
+          startLineId.getPosition(i).getClock());
+    } else if (i < endLineId.length()
+        && d == endLineId.getPosition(i).getInt()) {
+      position = new Position(d,
+          endLineId.getPosition(i).getReplica(),
+          endLineId.getPosition(i).getClock());
+    } else {
+      position = new Position(d, replica, clock);
+      ++ clock;
+    }
+
+    lineId.add(position);
+  }
+
+  return lineId;
+}
+
---
+++ span_logoot_test/utils.js
@@ -0,0 +1,165 @@
+function str_pad(input, pad_length, pad_string, pad_type) {
+    // Returns input string padded on the left or right to specified length
+    // with pad_string  
+    // 
+    // version: 1109.2015
+    // discuss at: http://phpjs.org/functions/str_pad
+    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // + namespaced by: Michael White (http://getsprink.com)
+    // +      input by: Marco van Oort
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+    // *     example 1: str_pad('Kevin van Zonneveld', 30, '-=',
+    // *                    'STR_PAD_LEFT');
+    // *     returns 1: '-=-=-=-=-=-Kevin van Zonneveld'
+    // *     example 2: str_pad('Kevin van Zonneveld', 30, '-', 'STR_PAD_BOTH');
+    // *     returns 2: '------Kevin van Zonneveld-----'
+    var half = '',
+        pad_to_go;
+ 
+    var str_pad_repeater = function (s, len) {
+        var collect = '',
+            i;
+ 
+        while (collect.length < len) {
+            collect += s;
+        }
+        collect = collect.substr(0, len);
+ 
+        return collect;
+    };
+ 
+    input += '';
+    pad_string = pad_string !== undefined ? pad_string : ' ';
+ 
+    if (pad_type != 'STR_PAD_LEFT'
+        && pad_type != 'STR_PAD_RIGHT'
+        && pad_type != 'STR_PAD_BOTH') {
+        pad_type = 'STR_PAD_RIGHT';
+    }
+    if ((pad_to_go = pad_length - input.length) > 0) {
+        if (pad_type == 'STR_PAD_LEFT') {
+            input = str_pad_repeater(pad_string, pad_to_go) + input;
+        } else if (pad_type == 'STR_PAD_RIGHT') {
+            input = input + str_pad_repeater(pad_string, pad_to_go);
+        } else if (pad_type == 'STR_PAD_BOTH') {
+            half = str_pad_repeater(pad_string, Math.ceil(pad_to_go / 2));
+            input = half + input + half;
+            input = input.substr(0, pad_length);
+        }
+    }
+ 
+    return input;
+}
+
+function str_split (string, split_length) {
+    // Convert a string to an array. If split_length is specified, break the
+    // string down into chunks each split_length characters long.  
+    // 
+    // version: 1109.2015
+    // discuss at: http://phpjs.org/functions/str_split
+    // +     original by: Martijn Wieringa
+    // +     improved by: Brett Zamir (http://brett-zamir.me)
+    // +     bugfixed by: Onno Marsman
+    // +      revised by: Theriault
+    // +        input by: Bjorn Roesbeke (http://www.bjornroesbeke.be/)
+    // +      revised by: Rafał Kukawski (http://blog.kukawski.pl/)
+    // *       example 1: str_split('Hello Friend', 3);
+    // *       returns 1: ['Hel', 'lo ', 'Fri', 'end']
+    if (split_length === null) {
+        split_length = 1;
+    }
+    if (string === null || split_length < 1) {
+        return false;
+    }
+    string += '';
+    var chunks = [],
+        pos = 0,
+        len = string.length;
+    while (pos < len) {
+        chunks.push(string.slice(pos, pos += split_length));
+    }
+ 
+    return chunks;
+}
+
+/*!
+ * \brief   Returns random value between min and max.
+ *
+ * \param   min Min range for random value.
+ * \param   max Max range for random value.
+ * \return  Random integer value in range <tt>[min..max]</tt>.
+ */
+function rand(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+/*!
+ * \brief   Binary Search on array.
+ * 
+ * Caution if they have no value equals to \c value in the array, this
+ * implemetation will return the first next uper value 
+ * Recursive implementation of binary search:
+ * http://en.wikipedia.org/wiki/Binary_search_algorithm
+ *
+ * \param   arr     The array over search.
+ * \param   value   The value to search.
+ * \param   low     The low value.
+ * \param   high    The high value.
+ * \param   compare Function using as order relation between values in the
+ *          array. The function would have same comportement than CompareTo in
+ *          java. If the function isn't give this will use "< > ==".
+ */
+var BINARYSEARCH_FROM_LEFT = 0;
+var BINARYSEARCH_FROM_RIGHT = 1;
+function binarySearch(arr, value, low, high, compare, from) {
+
+  if (high < low) {
+    if (from == BINARYSEARCH_FROM_LEFT) {
+      return high + 1;
+    } else if (from == BINARYSEARCH_FROM_RIGHT) {
+      return low;
+    }
+  }
+
+  var mid = Math.floor((low + high) / 2);
+  var compareValue = (compare) ? compare(arr[mid], value) : 0;
+  var isMidValueLower = (compare) ? (compareValue < 0) : (arr[mid] < value);
+  var isMidValueHigher = (compare) ? (compareValue > 0) : (arr[mid] > value);
+
+  if (isMidValueHigher) {
+    -- mid;
+    // console.log('left['+low+':'+mid+']');
+    return binarySearch(arr, value, low, mid, compare, BINARYSEARCH_FROM_LEFT);
+  } else if (isMidValueLower) {
+    ++ mid;
+    // console.log('right['+mid+':'+high+']');
+    return binarySearch(arr, value, mid, high, compare,BINARYSEARCH_FROM_RIGHT);
+  }
+
+  return mid;
+}
+
+//! \brief    String replace at function.
+/*
+function replaceAt(str, index, char) {
+  str.substr(0, index) + char + this.substr(index+char.length);
+}
+*/
+
+function getCaretPosition (ctrl) {
+  var CaretPos = 0; // IE Support
+
+  if (document.selection) {
+    ctrl.focus ();
+    var Sel = document.selection.createRange();
+    Sel.moveStart ('character', -ctrl.value.length);
+    CaretPos = Sel.text.length;
+  }
+
+  // Firefox support
+  else if (ctrl.selectionStart || ctrl.selectionStart == '0')
+    CaretPos = ctrl.selectionStart;
+
+  return (CaretPos);
+}
+
